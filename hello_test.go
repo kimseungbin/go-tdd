3b76125f4de3d87e644864d657cb580767ca7815
SeungBin Kim <seungbin0508@gmail.com>
2022-04-23 11:10:22 +0900

    Update hello_test.go to test Spanish output

--- hello_test.go
+++ hello_test.go
@@ -6,19 +6,25 @@ func TestHello(t *testing.T) {
 	assertCorrectMessage := func(tb testing.TB, got, want string) {
 		t.Helper()
 		if got != want {
-			t.Errorf("got %q want %Q", got, want)
+			t.Errorf("got %q want %q", got, want)
 		}
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'World'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
