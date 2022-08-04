54b52b449919f1189a7d6485d36045aa8f405dc1
SeungBin Kim <seungbin0508@gmail.com>
2022-08-04 15:20:08 +0900

    Add test for SumAllTails

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -53,3 +53,12 @@ func BenchmarkSumAll(b *testing.B) {
 		SumAll([]int{1, 2, 3}, []int{4, 5, 6}, []int{7, 8, 9})
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2, 3}, []int{4, 5, 6}, []int{7, 8, 9})
+	want := []int{5, 11, 17}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
