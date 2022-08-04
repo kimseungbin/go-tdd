43515eb9c2076841a92e44b9b278e3a0e5a0fb98
SeungBin Kim <seungbin0508@gmail.com>
2022-08-04 15:21:27 +0900

    Implement SumAllTails function

--- arrays/sum.go
+++ arrays/sum.go
@@ -15,5 +15,9 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 }
 
 func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
 	return
 }
