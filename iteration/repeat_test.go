106df455ed57ba7acf4779f6cbb609641464eb7b
SeungBin Kim <seungbin0508@gmail.com>
2022-08-04 13:25:34 +0900

    Refactor test for Repeat function
    The caller can specify how many times the character is repeated

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
@@ -15,13 +15,13 @@ func TestRepeat(t *testing.T) {
 }
 
 func ExampleRepeat() {
-	repeated := Repeat("b")
+	repeated := Repeat("b", 10)
 	fmt.Println(repeated)
-	// Output: bbbbb
+	// Output: bbbbbbbbbb
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 10000)
 	}
 }
