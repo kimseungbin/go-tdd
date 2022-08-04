f7e0630cc2ff0a7b74dd51ef9bbc96b9f3036fc3
SeungBin Kim <seungbin0508@gmail.com>
2022-08-04 15:07:40 +0900

    Implement SumAll function

--- arrays/sum.go
+++ arrays/sum.go
@@ -8,5 +8,8 @@ func Sum(numbers []int) (sum int) {
 }
 
 func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
 	return
 }
