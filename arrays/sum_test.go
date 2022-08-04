e7e0d4ded6c7d712a229b98ca0fabb26f563ca20
SeungBin Kim <seungbin0508@gmail.com>
2022-08-04 14:24:46 +0900

    Refactor test for Sum function to take slice too

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -6,25 +6,38 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
 }
 
 func ExampleSum() {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 	sum := Sum(numbers)
 	fmt.Println(sum)
 	// Output: 15
 }
 
 func BenchmarkSum(b *testing.B) {
-	numbers := [5]int{100, 200, 300, 400, 500}
+	numbers := []int{100, 200, 300, 400, 500}
 	for i := 0; i < b.N; i++ {
 		Sum(numbers)
 	}
