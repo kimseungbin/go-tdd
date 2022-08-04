55501b95300d4b0a415621a449faf222f0523eac
SeungBin Kim <seungbin0508@gmail.com>
2022-08-04 14:57:39 +0900

    Refactor test for SumAll function to use reflect.DeepEqual

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -2,6 +2,7 @@ package arrays
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -36,7 +37,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
