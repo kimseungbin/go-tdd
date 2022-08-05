4b7177061613ca3a0d1bca6d7153e241ab17c7ec
SeungBin Kim <seungbin0508@gmail.com>
2022-08-05 17:27:46 +0900

    Refactor TestArea to show name of shape and its struct

--- structs/shape_test.go
+++ structs/shape_test.go
@@ -35,25 +35,28 @@ func ExamplePerimeter() {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{
+		{name: "Rectangle", shape: Rectangle{
 			Width:  12,
 			Height: 6,
-		}, 72.0},
-		{Circle{Radius: 10.0}, 314.1592653589793},
-		{Triangle{
+		}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{
 			Base:   12,
 			Height: 6,
-		}, 36.0},
+		}, hasArea: 36.0},
 	}
 
 	for _, test := range areaTests {
-		got := test.shape.Area()
-		if got != test.want {
-			t.Errorf("got %g want %g", got, test.want)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			got := test.shape.Area()
+			if got != test.hasArea {
+				t.Errorf("%#v got %g want %g", test.shape, got, test.hasArea)
+			}
+		})
 	}
 }
 
