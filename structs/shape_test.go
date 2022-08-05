22177b9d6fe62a7e4897f62b798b2137479ac9b0
SeungBin Kim <seungbin0508@gmail.com>
2022-08-05 15:03:34 +0900

    Refactor TestArea to test Circle struct too

--- structs/shape_test.go
+++ structs/shape_test.go
@@ -29,17 +29,29 @@ func ExamplePerimeter() {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{
-		Width:  2.0,
-		Height: 2.0,
-	}
 
-	got := Area(rectangle)
-	want := 4.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{
+			Width:  12,
+			Height: 6,
+		}
+		got := Area(rectangle)
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{Radius: 10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
 
 func ExampleArea() {
