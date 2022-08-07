6fa2fac55b4c93d5826ede05d4f73d707a9a1599
SeungBin Kim <seungbin0508@gmail.com>
2022-08-07 18:04:20 +0900

    Refactor Wallet test to use helper func assertBalance

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -4,17 +4,21 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(tb testing.TB, wallet Wallet, want Bitcoin) {
+		tb.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
@@ -22,11 +26,6 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
