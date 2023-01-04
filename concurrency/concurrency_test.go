043c29352000bc684b58b39a62a40984d7f79263
SeungBin Kim <luxorienskim@gmail.com>
2023-01-04 11:24:31 +0900

    test: add benchmark test for CheckWebsites function

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -37,3 +37,14 @@ func slowStubWebsiteChecker(_ string) bool {
 	time.Sleep(20 * time.Millisecond)
 	return true
 }
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < b.N; i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
