39e49ce32b5d1959ecadd6c138867b20529ee4c5
SeungBin Kim <luxorienskim@gmail.com>
2023-01-22 16:36:15 +0900

    feat: implement Racer func

--- select/racer.go
+++ select/racer.go
@@ -1,5 +1,22 @@
 package racer
 
+import (
+	"net/http"
+	"time"
+)
+
 func Racer(a, b string) (winner string) {
-	return
+	startA := time.Now()
+	http.Get(a)
+	durationA := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	durationB := time.Since(startB)
+
+	if durationA < durationB {
+		return a
+	}
+
+	return b
 }
