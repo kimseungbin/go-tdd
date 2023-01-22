2642d6870458ba48d9be6c1842997ab440184afb
SeungBin Kim <luxorienskim@gmail.com>
2023-01-22 17:02:15 +0900

    refactor: add measureResponseTimer func to reduce duplicate code

--- select/racer.go
+++ select/racer.go
@@ -6,17 +6,18 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	durationA := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	durationB := time.Since(startB)
+	durationA := measureResponseTimer(a)
+	durationB := measureResponseTimer(b)
 
 	if durationA < durationB {
 		return a
 	}
 
 	return b
+}
+
+func measureResponseTimer(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
