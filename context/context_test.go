54bbe666be9a6ac164364fe698653eb83be8b9a4
SeungBin Kim <luxorienskim@gmail.com>
2023-01-26 11:34:42 +0900

    test: rewrite default ase

--- context/context_test.go
+++ context/context_test.go
@@ -23,28 +23,36 @@ func (s *SpyStore) Cancel() {
 }
 
 func TestServer(t *testing.T) {
-	data := "hello, world"
-	server := Server(&SpyStore{response: data})
+	t.Run("returns data from store", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		server := Server(store)
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
 
-	server.ServeHTTP(response, request)
+		server.ServeHTTP(response, request)
 
-	if response.Body.String() != data {
-		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-	}
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+
+		if store.cancelled {
+			t.Error("it should not have cancelled the store")
+		}
+	})
 
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
 		data := "hello, world"
 		store := &SpyStore{response: data}
-		server = Server(store)
+		server := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 
 		cancellingCtx, cancelFunc := context.WithCancel(request.Context())
 		time.AfterFunc(5*time.Millisecond, cancelFunc)
 		request = request.WithContext(cancellingCtx)
+		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
