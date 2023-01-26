53ae537138dfa3f3736f11484270873edbfe0a21
SeungBin Kim <luxorienskim@gmail.com>
2023-01-26 11:57:18 +0900

    test: implement assertion helper

--- context/context_test.go
+++ context/context_test.go
@@ -41,7 +41,7 @@ func TestServer(t *testing.T) {
 	data := "hello, world"
 
 	t.Run("returns data from store", func(t *testing.T) {
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		server := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -53,13 +53,11 @@ func TestServer(t *testing.T) {
 			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
 		}
 
-		if store.cancelled {
-			t.Error("it should not have cancelled the store")
-		}
+		store.assertWasNotCancelled()
 	})
 
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		server := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -71,8 +69,6 @@ func TestServer(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
-		if !store.cancelled {
-			t.Error("store was not told to cancel")
-		}
+		store.assertWasCancelled()
 	})
 }
