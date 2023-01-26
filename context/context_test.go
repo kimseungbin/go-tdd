15af0364b95a833d093d6cba3e7dc2301d38e7d8
SeungBin Kim <luxorienskim@gmail.com>
2023-01-26 14:31:40 +0900

    test: implement SpyResponseWriter to spy on response

--- context/context_test.go
+++ context/context_test.go
@@ -2,6 +2,7 @@ package context
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -44,6 +45,24 @@ func (s *SpyStore) Cancel() {
 
 }
 
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
+
 func TestServer(t *testing.T) {
 	data := "hello, world"
 
@@ -58,6 +77,25 @@ func TestServer(t *testing.T) {
 
 		if response.Body.String() != data {
 			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		server := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancelFunc := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancelFunc)
+		request = request.WithContext(cancellingCtx)
+
+		response := &SpyResponseWriter{}
+
+		server.ServeHTTP(response, request)
+
+		if response.written {
+			t.Error("a response should not have been written")
 		}
 	})
 }
