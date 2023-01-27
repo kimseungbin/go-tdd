ff2fbac9523f0fce6a8f2ff00d54829a81d9c2fd
SeungBin Kim <luxorienskim@gmail.com>
2023-01-27 17:15:47 +0900

    feat: add error return

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -7,11 +7,11 @@ import (
 type Post struct {
 }
 
-func NewPostsFromFS(fileSystem fs.FS) []Post {
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	dirEntries, _ := fs.ReadDir(fileSystem, ".")
 	var posts []Post
 	for range dirEntries {
 		posts = append(posts, Post{})
 	}
-	return posts
+	return posts, nil
 }
