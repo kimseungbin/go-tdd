80ccc81b73b1de3fc6f63f2a800cd63135cca398
SeungBin Kim <luxorienskim@gmail.com>
2023-01-31 10:43:39 +0900

    test: add DeepEqual check on post

--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -1,6 +1,7 @@
 package blogposts_test
 
 import (
+	"reflect"
 	"tdd/blogposts"
 	"testing"
 	"testing/fstest"
@@ -8,11 +9,18 @@ import (
 
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
 
 	if err != nil {
 		t.Fatal(err)
