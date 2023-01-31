d35a92b7633e5cbe6278e2ad7fb6eb99f7bf40cc
SeungBin Kim <luxorienskim@gmail.com>
2023-01-31 10:59:45 +0900

    feat: getPost func

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -1,6 +1,7 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
 )
 
@@ -15,4 +16,20 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		posts = append(posts, Post{})
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData[7:])}
+	return post, nil
 }
