e14804937972f2d80eeaa6a9e8a215bc2c269e54
SeungBin Kim <luxorienskim@gmail.com>
2023-09-18 14:28:01 +0900

    feat: implement minimum to pass TestRender

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -1,6 +1,9 @@
 package blogrenderer
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Post struct {
 	Title, Description, Body string
@@ -8,5 +11,9 @@ type Post struct {
 }
 
 func Render(writer io.Writer, post Post) error {
+	_, err := fmt.Fprintf(writer, "<h1>%s</h1>", post.Title)
+	if err != nil {
+		return err
+	}
 	return nil
 }
