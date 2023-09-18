360d992a8ba7263af6031a04343a1c8f7513452d
SeungBin Kim <luxorienskim@gmail.com>
2023-09-18 19:56:41 +0900

    test: add BenchmarkRender test

--- blogrenderer/blogrenderer_test.go
+++ blogrenderer/blogrenderer_test.go
@@ -3,6 +3,7 @@ package blogrenderer_test
 import (
 	"bytes"
 	approvals "github.com/approvals/go-approval-tests"
+	"io"
 	"tdd/blogrenderer"
 	"testing"
 )
@@ -26,4 +27,20 @@ func TestRender(t *testing.T) {
 
 		approvals.VerifyString(t, buffer.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Description: "This is a post",
+			Body:        "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		blogrenderer.Render(io.Discard, aPost)
+	}
 }
